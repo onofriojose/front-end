4105ab8aafbad5c2fa83324b1753170f1d4456f1
onomorph <onomorph@gmail.com>
2023-04-19 20:42:54 -0400

    agregar cambios

--- Evaluacion Final copy/js/javascript.js
+++ Evaluacion Final copy/js/javascript.js
@@ -1,20 +1,61 @@
-$(document).ready(function () {
-  fetch("https://digimon-api.vercel.app/api/digimon")
-    .then((respuesta) => respuesta.json())
-    .then((datos) => {
-      console.log(datos);
-      datos.forEach((digimon) => {
-        let opcion = $("<option></option>").text(digimon.name).val(digimon.img).data("level", digimon.level); //option tomara los valores de cada digimon en la lista
-        $("#seleccion").append(opcion); //a medida que se recorre la lista se van agregando opciones a la lista desplegable
+document.addEventListener("DOMContentLoaded", function (event) {
+  $(document).ready(function () {
+    fetch("https://digimon-api.vercel.app/api/digimon")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        console.log(datos);
+        mostrar(datos);
+        let formulario = document.querySelector("form");
+        formulario.addEventListener("submit", buscarDigimon);
       });
-      $("#seleccion").change(function () {
-        //evento para cuando se cambie de opcion en el select
-        let imgSrc = $(this).val(); //val tiene el valor de digimon.img
-        let nombre = $(this).find(":selected").text();
-        let nivel = $(this).find(":selected").data("level");
-        $("#imagen").attr("src", imgSrc);
-        $("#titulo").text(nombre);
-        $("#level").text(nivel); // Muestra el valor del atributo 'data-level'
-      });
-    });
+    function mostrar(response) {
+      let tarjeta = document.getElementById("tarjeta");
+      tarjeta.innerHTML = "";
+      if (response.length > 1) {
+        for (let temp of response) {
+          tarjeta.innerHTML += `
+        <div class="card mt-5 mx-auto col-sm-12" style="width: 18rem;">
+          <img src="${temp.img}" id="${temp.name}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-center">${temp.name}</h5>
+            <p id="level" class="card-text text-center">${temp.level}</p>
+          </div>
+        </div>
+        `;
+        }
+      } else {
+        for (let temp of response) {
+          tarjeta.innerHTML += `
+      <div class="card mt-5 mx-auto col-sm-12" style="width: 18rem;">
+        <img src="${temp.img}" id="${temp.name}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-center">${temp.name}</h5>
+          <p id="level" class="card-text text-center">${temp.level}</p>
+          <a href="index.html" class="btn btn-primary">Volver</a>
+        </div>
+      </div>
+      `;
+        }
+      }
+    }
+    function buscarDigimon(event) {
+      event.preventDefault(); // evitar que se recargue la página
+      let busqueda = document.querySelector('input[type="search"]');
+      let nombre = busqueda.value.trim();
+      fetch(`https://digimon-api.vercel.app/api/digimon/name/${nombre}`)
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          mostrar(datos);
+          let boton = document.createElement("button"); // crear boton en el index donde se muestra la carta
+          boton.innerHTML = "Volver";
+          boton.classList.add("d-flex", "justify-content-center"); // agregar clases usando classList.add()
+          boton.onclick = function () {
+            location.reload();
+          };
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
 });
